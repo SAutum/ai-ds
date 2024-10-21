5687f89c740200ad94e07a42597444fd15027426
Hongli Lin <hongli.lin@hhu.de>
2024-10-21 21:44:02 +0200

    hongli: rdb_e00

--- rdb/hongli/assignment-0/data/docker_build.sh
+++ rdb/hongli/assignment-0/data/docker_build.sh
@@ -1,8 +1,8 @@
 # Build docker image named "rddaimage" with id of current user to avoid
 # file permission conflicts.
 docker build \
-    --build-arg MY_USER_ID=$(id -u) \
-    --build-arg MY_GROUP_ID=$(id -g) \
+    --build-arg MY_USER_ID=1000 \
+    --build-arg MY_GROUP_ID=1000 \
     --build-arg MY_USER_NAME=user$(shuf -i 10000000-99999999 -n 1) \
     --progress=plain \
     -t rddaimage .
