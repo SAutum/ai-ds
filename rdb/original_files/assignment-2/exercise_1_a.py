f556132b6698ec9d565706e14586ce58674f8537
Hongli Lin <yoursilf_134@hotmail.com>
2024-10-22 17:07:48 +0200

    rdb: exercise02_original files

---
+++ rdb/original_files/assignment-2/exercise_1_a.py
@@ -0,0 +1,5 @@
+def get_query() -> str:
+    # Your solution goes here.
+
+    # his query is just a placeholder.
+    return "SELECT attribute FROM relation"
---
+++ rdb/original_files/assignment-2/test_exercise_2.py
@@ -0,0 +1,70 @@
+import sqlite3
+import exercise_2
+import os
+
+
+def test() -> None:
+    # Remove database file
+    db_filename = "mydatabase.db"
+
+    try:
+        os.remove(db_filename)
+    except FileNotFoundError:
+        pass
+
+    # Create new database
+    exercise_2.create_database()
+
+    # Check if database file has been created
+    assert os.path.isfile(db_filename), f"Could not find file named {db_filename}"
+
+    # Open database
+    db = sqlite3.connect(db_filename)
+
+    cursor = db.cursor()
+
+    allowed_table_name_chars = set(
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+    )
+
+    # Check all tables in database
+    for table_info in cursor.execute("SELECT * FROM sqlite_master").fetchall():
+
+        # Skip indexes and such
+        if table_info[0] != "table":
+            continue
+
+        table_name = table_info[1]
+
+        # Skip internal table
+        if table_name == "sqlite_sequence":
+            continue
+
+        # WARNING!
+        # Building queries using format strings is generally a source of
+        # SQL injection vulnerabilities and should generally not be done,
+        # so we ensure that the table name only consists of safe characters.
+        assert all(c in allowed_table_name_chars for c in table_name)
+
+        rows = cursor.execute(f"SELECT * FROM {table_name}").fetchall()
+
+        print(f'The table "{table_name}" has {len(rows)} rows.')
+        print("The first 10 rows are:")
+        for row in rows[:10]:
+            print(row)
+
+        assert len(rows) == 1000, "Table must have 1000 rows of data!"
+
+        assert len(rows[0]) >= 2, "Table must have at least 2 columns!"
+
+    try:
+        os.remove(db_filename)
+    except FileNotFoundError:
+        pass
+
+if __name__ == "__main__":
+    test()
+    import zipme
+    zipme.finish(__file__, max_size=2000)
+
+# 62757101723200000000
