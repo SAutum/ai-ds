b8e5e922d617ad0cfb15a42da00654b34f4a95ed
Hongli Lin <lin.hongli@hhu.com>
2024-10-30 13:12:00 +0100

    rdb: exercise03_original_files

--- rdb/original_files/assignment-2/exercise_1_a.py
+++ rdb/original_files/assignment-2/exercise_1_a.py
@@ -2,4 +2,4 @@ def get_query() -> str:
     # Your solution goes here.
 
     # his query is just a placeholder.
-    return "SELECT attribute FROM relation"
+    return "SELECT DISTINCT productname FROM relation"
---
+++ rdb/original_files/assignment-3/exercise_1_c.py
@@ -0,0 +1,5 @@
+def get_query() -> str:
+    # Your solution goes here.
+
+    # This is just a placeholder.
+    return "SELECT money FROM bank WHERE security = 'weak'"
