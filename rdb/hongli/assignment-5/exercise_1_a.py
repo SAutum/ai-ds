148313646b248bc5a16e549079399b7780208721
Hongli Lin <lin.hongli@hhu.de>
2024-11-15 16:30:13 +0100

    hongli:rdb_e05_v00

--- rdb/hongli/assignment-5/exercise_1_a.py
+++ rdb/hongli/assignment-5/exercise_1_a.py
@@ -17,12 +17,16 @@ def join(relation1: Relation, relation2: Relation) -> Relation:
     relation_len1 = relation_len(relation=relation1)
     relation_len2 = relation_len(relation=relation2)
 
+    # pick the one with smaller num of rows to
     if relation_len1 < relation_len2:
         build_input_R = relation1
+        probe_input_S = relation2
     else:
         build_input_R = relation2
+        probe_input_S = relation1
 
-    # initialize the hash table, store tuples in the relation to R
+    # initialize the hash table, store tuples in the relation as python dictionary
+    # to the hash table
     for i in range(relation_len(build_input_R)):
         """
         tuple can be hashed, so I set the common attribute values as key
@@ -30,19 +34,40 @@ def join(relation1: Relation, relation2: Relation) -> Relation:
         the same key: tuple()
         """
         key = tuple([build_input_R[value][i] for value in common_attributes])
-        # set the combined attribute values as value
-        value = tuple([build_input_R[value][i] for value in combined_attributes])
+        """
+        set the value as list of dictionaries. I tried tuple, but the
+        attribute-value pair doesn't really match anymore (because the attributes are
+        stored as set)
+        """
+        value = {}
+        for attribute in build_input_R.keys():
+            value[attribute] = build_input_R[attribute][i]
         # insert the tuple to build input R
         insert_tuple(hash_table=hash_table, key=key, value=value)
-    print(hash_table)
+
+    # create an empty result relation
+    result: Relation = {attribute: [] for attribute in combined_attributes}
+
+    # iterate through S
+    for i in range(relation_len(probe_input_S)):
+        key = tuple([probe_input_S[value][i] for value in common_attributes])
+
+        # if we can find the key in the hash table, do a cross join
+        if hash_table.get(key) != None:
+            for r_value in hash_table[key]:
+                for attribute in combined_attributes:
+                    if r_value.get(attribute) != None:
+                        result[attribute].append(r_value[attribute])
+                    else:
+                        result[attribute].append(probe_input_S[attribute][i])
+    return result
+
 
 def insert_tuple(hash_table, key, value):
     if hash_table.get(key) == None:
         hash_table[key] = [value]
-
     else:
         hash_table[key].append(value)
-
 
 
 def relation_len(relation: Relation) -> int:
